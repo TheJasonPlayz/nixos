d1f33480ed97618e5b9172283c71c3975960dc8b
Jason Whitman <thejasonplayz@gmail.com>
2025-02-01 18:33:59 -0700

    add boot.nix

---
+++ hosts/pc/boot.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/f1dd5b31-d987-4d01-a226-84c8c8fad884";
+      fsType = "ext4";
+    };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/05f07eec-5305-4006-9eee-1a5840592ea4";
+      fsType = "xfs";
+    };
+  };
+
+  boot.supportedFilesystems = {
+    xfs = true;
+  };
+  boot.loader = {
+    timeout = 15;
+    grub = {
+      enable = true;
+      device = "/dev/disk/by-uuid/c913de29-e9e7-4b88-bdfd-321cf6f50b31";
+    };
+  };
+}
