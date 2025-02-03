e4dfbe365969265a082be6d17e9799bfbd9e8dd8
Jason Whitman <thejasonplayz@gmail.com>
2025-02-03 09:58:02 -0700

    local-perms.sh change order

--- scripts/local-perms.sh
+++ scripts/local-perms.sh
@@ -1,5 +1,5 @@
 PWD=$(pwd)
 
 find "$PWD" -type d | grep -v .git | xargs -L1 sudo chmod 755
-find "$PWD" | grep -v .git | xargs -L1 sudo chown jasonw:users 
 find "$PWD" -type f | grep -v .git | xargs -L1 sudo chmod 666
+find "$PWD" | grep -v .git | xargs -L1 sudo chown jasonw:users 
