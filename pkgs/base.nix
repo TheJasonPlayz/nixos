2df05530c7498534a2950073c89d390116aaeb5a
Jason Whitman <thejasonplayz@gmail.com>
2025-02-12 20:28:23 -0700

    Updates

--- pkgs/base.nix
+++ pkgs/base.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, config, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ rsync rclone wget sops age jq unzip fd parted libimobiledevice usbmuxd git];
+  environment.systemPackages = with pkgs; [ rsync rclone wget sops gpg jq unzip fd parted libimobiledevice usbmuxd git];
 }
